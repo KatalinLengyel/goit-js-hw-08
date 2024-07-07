f37a202f20df471a5053160cace7a18415eb2aeb
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-07-07 10:53:27 +0300

    Adjustments

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,37 +4,46 @@ const form = document.querySelector("form");
 const email = document.querySelector("input");
 const message = document.querySelector("textarea");
 
-let inputState = localStorage.getItem("feedback-form-state");
-inputState = JSON.parse(inputState || '{}');
+const getLocalStorageValue = () => {
+    let inputState = localStorage.getItem("feedback-form-state");
+    if (inputState) {
+        inputState = JSON.parse(inputState);
+        return inputState;
+    }
+    return {};
+}
+
+const inputState = getLocalStorageValue()
+
 if (inputState.email) {
     email.value = inputState.email;
 }
 
 if (inputState.message) {
     message.value = inputState.message;
-}
+};
 
 const storeInput = throttle((currentInput) => {
     
     const name = currentInput.target.name;
     const value = currentInput.target.value;
-    let currentState = localStorage.getItem("feedback-form-state");
-    currentState = JSON.parse(currentState || '{}');
+    const currentState = getLocalStorageValue();
     currentState[name] = value;
-    console.log(currentState);
     localStorage.setItem("feedback-form-state", JSON.stringify(currentState))
 }, 500);
 
-email.addEventListener("keyup", storeInput);
-
-message.addEventListener("keyup", storeInput);
-
-form.addEventListener("submit", (event) => {
+const onSubmitForm = (event) => {
     event.preventDefault();
     console.log(`Email: ${email.value}`);
     console.log(`Message: ${message.value}`);
     localStorage.removeItem("feedback-form-state");
     email.value = "";
     message.value = "";
-})
+}
+
+email.addEventListener("keyup", storeInput);
+
+message.addEventListener("keyup", storeInput);
+
+form.addEventListener("submit", onSubmitForm);
 
